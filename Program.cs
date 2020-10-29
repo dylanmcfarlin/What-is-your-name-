1ef7ed62fe963f49d81e6e19e42f9839ff39ded3
Dylan McFarlin <dylanmcfarlin@Dylans-MacBook-Pro.local>
2020-10-29 16:12:11 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+// Dylan McFarlin
+// Date revised: 9-2-20
+// Mini Challenge #1
+namespace MiniChallenge_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("WHat is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
